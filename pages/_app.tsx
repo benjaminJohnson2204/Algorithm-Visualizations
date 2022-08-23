3326e7ee68e713fbf9b2a6b4faf395439fea22f8
Benjamin Johnson <ben332004@gmail.com>
2022-08-23 13:21:41 -0700

    Add header to navigate problems and topics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SSRProvider } from "react-bootstrap";
+import SiteHeader from "../components/SiteHeader";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SSRProvider>
+      <div>
+        <Head>
+          <title>Algorithm Visualizations</title>
+          <meta
+            name="description"
+            content="Visualization tool for various algorithms"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <SiteHeader />
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </SSRProvider>
+  );
 }
 
 export default MyApp;
